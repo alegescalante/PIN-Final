cc96cdbdf77897198dc4ad4fdd66c5f26cd523d5
Alan-Puky <alanepeloa@gmail.com>
2024-04-24 01:07:12 +0000

    variables

--- 2-eks/main.tf
+++ 2-eks/main.tf
@@ -6,17 +6,17 @@ provider "kubernetes" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  name = "eks-cluster"
+  name   = "eks-cluster"
   region = "us-east-1"
 
   #cluster_name = "eks-cluster-${random_string.suffix.result}"
 
   vpc_cidr = "10.0.0.0/16"
-  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs      = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-    
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
 }
 #resource "random_string" "suffix" {
 #  length  = 8
--- 2-eks/network.tf
+++ 2-eks/network.tf
@@ -8,18 +8,18 @@ module "vpc" {
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
-  
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
+    "kubernetes.io/role/elb"              = 1
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
+    "kubernetes.io/role/internal-elb"     = 1
   }
 }
